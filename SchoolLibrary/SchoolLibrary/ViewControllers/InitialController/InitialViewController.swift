21487027bf5495463b069299e8619c41f37002e9
RadiKovachev <119615784+RadiKovachevDev@users.noreply.github.com>
2022-12-01 19:04:32 +0200

    Create app structure (#1)

---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/InitialController/InitialViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InitialViewController.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 1.12.22.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/ProvidedControllers/ProvidedController/ProvidedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProvidedViewController.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 1.12.22.
+//
+
+import UIKit
+
+class ProvidedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
