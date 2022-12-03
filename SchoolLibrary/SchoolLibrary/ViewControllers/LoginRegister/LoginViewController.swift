192002c7d83279c54d20fd34718d63fa1012482c
RadiKovachev <119615784+RadiKovachevDev@users.noreply.github.com>
2022-12-03 20:17:18 +0200

    Create navigation register and login screen. Design login and register screen. (#2)

---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/LoginRegister/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 3.12.22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+//        self.performSegue(withIdentifier: "toRegisterSegue", sender: nil)
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "loginToAppSegue", sender: nil)
+    }
+    
+}
---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/LoginRegister/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 3.12.22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func createAccountButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "registerToAppSegue", sender: nil)
+    }
+}
