d1af34ac58558d0a18c7b151bee5762bfb6bfbe2
Radi Kovachev <radi.kovachev.dev@gmail.com>
2022-12-10 18:48:45 +0200

    create enum category, structure users, fake database

---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/Cells/BookCell/BookTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BookTableViewCell.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 10.12.22.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var smallDescriptionLabel: UILabel!
+    @IBOutlet weak var publisherLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
