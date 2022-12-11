cbc2ad63670ae4de8116370e529e2cc6184d5c67
Radi Kovachev <radi.kovachev.dev@gmail.com>
2022-12-11 18:12:20 +0200

    set up settings screen

---
+++ SchoolLibrary/SchoolLibrary/ViewControllers/SettingsControllers/SettingsController/SettingAppTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SettingAppTableViewCell.swift
+//  SchoolLibrary
+//
+//  Created by Radi on 11.12.22.
+//
+
+import UIKit
+
+class SettingAppTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
